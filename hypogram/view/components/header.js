da1e361f86e11afcc6e38614be5ad13ec34569af
Carlos Magalhães <65139043+carlosedsmagalhaes@users.noreply.github.com>
2024-10-29 20:19:05 -0300

    Alterações feitas pela andressatoledo, ajustes nas imagens e redirecionamentos

--- hypogram/view/components/header.js
+++ hypogram/view/components/header.js
@@ -1,34 +1,34 @@
 export function createHeader() {
-    const header = document.createElement('header');
-    header.innerHTML = `
+  const header = document.createElement("header");
+  header.innerHTML = `
     <div class="header-container">
         <img>
         <nav>
-            <a href="index.html">Inicio</a>
+            <a href="index.html">Início</a>
             <a href="sobre.html">Sobre</a>
             <a href="aprendaScrum.html">Aprenda Scrum</a>
             <a href="selecaoQuestionario.html" class="show">Questionários</a>
         </nav>
         <div id="botoes">
             <button class="show">Certificado</button>
-            <button>Entrar</button>
+            <button onclick="abrirTela('./login.html')">Entrar</button>
         </div>
     </div>
     `;
 
-    const img = header.querySelector('img');
-    img.src = '../public/imgs/logotipo.svg';
-    img.alt = "Logotipo do curso";
+  const img = header.querySelector("img");
+  img.src = "../public/imgs/logotipo.svg";
+  img.alt = "Logotipo do curso";
 
-    let login = true; 
-    
-    if (!login) {
-        const aparecer = header.querySelectorAll('.show'); 
-        aparecer.forEach(elemento => {
-            elemento.style.display = 'none'; 
-        });
-    }
+  let login = true;
 
-    const main = document.querySelector('main');
-    document.body.insertBefore(header, main);
+  if (!login) {
+    const aparecer = header.querySelectorAll(".show");
+    aparecer.forEach((elemento) => {
+      elemento.style.display = "none";
+    });
+  }
+
+  const main = document.querySelector("main");
+  document.body.insertBefore(header, main);
 }
