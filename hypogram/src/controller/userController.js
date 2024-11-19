4373c5e9a4869c16ce1c97afefe9d7f9d1d76f3b
Gustavo Moraes <guhms@outlook.com>
2024-11-19 11:26:31 -0300

    ajuste login

--- hypogram/src/controller/userController.js
+++ hypogram/src/controller/userController.js
@@ -1,28 +1,6 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
-const criarUsuario = async (req, res) => {
-  const { nome, email, senha } = req.body;
-  const status = "ativo";
-
-  try {
-    // Criptografando a senha antes de armazenar no banco
-    const hashedSenha = await bcrypt.hash(senha, 10);
-
-    // Chama o método para criar o usuário com a senha criptografada
-    await userModel.criarUsuario(nome, email, hashedSenha, status);
-
-    // Redireciona para a página de login após a criação
-    return res.redirect("/login");
-  } catch (error) {
-    console.error(error);
-    // Renderiza a página de erro ou envia uma mensagem de erro
-    return res.render("erro", {
-      mensagem: "Erro ao criar usuário. Tente novamente.",
-    });
-  }
-};
-
 const buscarUsuarioPorId = async (req, res) => {
   const { id } = req.params;
   try {
@@ -31,6 +9,21 @@ const buscarUsuarioPorId = async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).send("Erro ao buscar usuário");
+  }
+};
+
+const criarUsuario = async (req, res) => {
+  const { nome, email, senha, status } = req.body;
+  console.log("Tentando criar usuário com email:", email); // Log de entrada do email
+
+  try {
+    // Cria o usuário no banco de dados
+    const usuario = await userModel.criarUsuario(nome, email, senha, status);
+    console.log("Usuário criado com sucesso:", usuario);
+    return res.render("login", { mensagem: "Usuário criado com sucesso." });
+  } catch (error) {
+    console.error("Erro ao criar usuário:", error);
+    return res.render("erro", { mensagem: "Erro interno do servidor." });
   }
 };
 
@@ -57,12 +50,14 @@ const loginUser = async (req, res) => {
     if (!isPasswordCorrect) {
       console.log("Senha incorreta.");
       // Envia uma mensagem de erro para a página de login
-      return res.render("login", { erro: "E-mail ou senha incorretos." });
+      return res.render("login", {
+        erro: "E-mail ou senha incorretos.",
+      });
     }
-
-    console.log("Login bem-sucedido.");
-    // Renderiza a página de destino após o login
-    return res.render("aprendaScrum"); // Substitua por sua página de destino
+    localStorage.setItem("usuarioLogado");
+    return res.render("aprendaScrum", {
+      mensagem: "Login realizado com sucesso.",
+    });
   } catch (error) {
     console.error("Erro ao fazer login:", error);
     // Renderiza a página de erro se houver erro no login
